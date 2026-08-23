6d38acc493451aa6b71a4c1bd14e15722e7fccfb
Harsh Sharma <harshsharma07893@gmail.com>
2026-08-24 01:22:24 +0530

    Complete listing CRUD operations

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
-
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,6 +20,8 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
 
 app.get("/",(req,res)=>{
     res.send("Hi i am root");
@@ -36,11 +38,47 @@ app.get("/listings", async (req, res) => {
   }
 });
 
-//Show Route
+// New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+
+// show route
 app.get("/listings/:id", async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
-    res.render('listings/show',{listing});
+    res.render("listings/show.ejs",{listing});
+});
+
+// Create Route
+app.post("/listings", async(req,res)=>{
+     const newListing = new Listing(req.body.listing);
+     await newListing.save();
+     res.redirect("/listings");
+});            
+
+//edit route
+app.get("/listings/:id/edit", async(req,res)=>{
+     let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+
+// Update route
+app.put("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.                                                                               body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+// Delete route
+app.delete("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    let deletetedlisting =  await Listing.findByIdAndDelete(id);
+    console.log(deletetedlisting);
+    res.redirect("/listings"); 
 });
 
 // app.get("/testListing",async (req,res)=>{
