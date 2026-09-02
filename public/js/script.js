50178c9f2584c762e5a676a4c408985f1221c920
Harsh Sharma <harshsharma07893@gmail.com>
2026-09-02 14:44:40 +0530

    Implement client side validations

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,53 @@
-(() => {
-    'use strict';
-    const forms = document.querySelectorAll('.needs-validation');
+const forms = document.querySelectorAll(".needs-validation");
 
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
+forms.forEach((form) => {
 
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
+    const inputs = form.querySelectorAll("input, textarea, select");
+
+    inputs.forEach((input) => {
+
+        input.addEventListener("input", () => {
+
+            if (input.value.trim() === "") {
+                input.classList.remove("is-valid");
+                input.classList.add("is-invalid");
+            } else {
+                input.classList.remove("is-invalid");
+                input.classList.add("is-valid");
             }
 
-            form.classList.add('was-validated');
+        });
 
-        }, false);
     });
-})();
+
+
+    form.addEventListener("submit", (event) => {
+
+        let valid = true;
+
+        inputs.forEach((input) => {
+
+            if (input.required && input.value.trim() === "") {
+
+                input.classList.remove("is-valid");
+                input.classList.add("is-invalid");
+
+                valid = false;
+
+            } else {
+
+                input.classList.remove("is-invalid");
+                input.classList.add("is-valid");
+
+            }
+
+        });
+
+
+        if (!valid) {
+            event.preventDefault();
+        }
+
+    });
+
+});
