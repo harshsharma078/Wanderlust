2f622094a3fb0a3379c83c449091a25b20e896ab
Harsh Sharma <harshsharma07893@gmail.com>
2026-09-02 16:18:01 +0530

    Add Joi validation and fix error handling

---
+++ utils/ExpressError.js
@@ -0,0 +1,10 @@
+class ExpressError extends Error {
+
+    constructor(statuscode, message) {
+        super(message);
+        this.statuscode = statuscode;
+    }
+
+}
+
+module.exports = ExpressError;
